dd3014d0ee7489fba9789e16f6383d67cc147425
potts4116 <potts4116@gmail.com>
2017-11-15 19:43:46 -0500

    Initial commit

---
+++ Assets/Explosion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject Grenade;
+    public float power = 10.0f;
+    public float radius = 5.0f;
+    public float upForce = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        if (Grenade == enabled)
+        {
+            Invoke("Detonate", 5);
+        }
+	}
+
+    void Detonate()
+    {
+        Vector3 explosionPosition = Grenade.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPosition, radius, upForce, ForceMode.Impulse);
+            }
+        }
+        Invoke("Destroy", 5);
+        
+    }
+
+    void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/GrenadeThrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrow : MonoBehaviour {
+
+    public GameObject grenade;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<>
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        
+    }
+}
